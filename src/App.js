eb3ca70e7ac508251b8abc73f19d9a97df3e8241
hancco05 <s.carmonawright@gmail.com>
2025-02-01 19:47:35 -0300

    tercer commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css'; // Estilos globales
 import Home from './components/Home'; // Importamos el componente Home
 import Benefits from "./components/Benefits";
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Home /> {/* Usamos el componente Home aquí */}
+      <Home /> 
       <Benefits />
+      <Footer /> 
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2025 HappyPets. Todos los derechos reservados.</p>
+      <p>
+        Síguenos en:
+        <a href="#"> Facebook</a> | 
+        <a href="#"> Instagram</a> | 
+        <a href="#"> Twitter</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
